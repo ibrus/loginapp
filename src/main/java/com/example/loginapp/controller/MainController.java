ee30e41f9d93d52eb384b428a60094d810dd106e
ibrus <brus.andreevich@gmail.com>
2020-04-24 11:56:37 +0300

    validation added

--- src/main/java/com/example/loginapp/controller/MainController.java
+++ src/main/java/com/example/loginapp/controller/MainController.java
@@ -2,37 +2,39 @@ package com.example.loginapp.controller;
 
 import com.example.loginapp.model.User;
 import com.example.loginapp.service.UserService;
-import com.example.loginapp.utility.UserValidator;
+//import com.example.loginapp.utility.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @Controller
 public class MainController {
     @Autowired
     private UserService userService;
-
-    @Autowired
-    private UserValidator userValidator;
+//
+//    @Autowired
+//    private UserValidator userValidator;
 
     @GetMapping("/registration")
     public String registration(Model model) {
-        model.addAttribute("userForm", new User());
+        model.addAttribute("user", new User());
 
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
-        userValidator.validate(userForm, bindingResult);
+    public String registration(@Valid @ModelAttribute User user, BindingResult bindingResult) {
+//        userValidator.validate(user, bindingResult);
 
         if (bindingResult.hasErrors()) {
             return "registration";
         }
 
-        userService.save(userForm);
+        userService.save(user);
 
         return "redirect:/login";
     }
--- src/main/java/com/example/loginapp/model/User.java
+++ src/main/java/com/example/loginapp/model/User.java
@@ -1,7 +1,8 @@
 package com.example.loginapp.model;
 
+//import javax.validation.constraints.NotNull;
+//import javax.validation.constraints.Size;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -14,8 +15,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+//    @NotNull(message="field should not be empty")
+//    @Size(min=1, max=32, message="Name must be between 6 and 32 characters")
     private String username;
 
+//    @NotNull(message="field should not be empty")
+//    @Size(min=6, max=32, message="Password must be between 6 and 32 characters")
     private String password;
 
     @Transient
